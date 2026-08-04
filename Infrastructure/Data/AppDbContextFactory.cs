916f63c1574b11b5980a997a561be0224bccfa6b
Ewerest <Ewerest@MakuOSV6PC-8609>
2026-08-04 17:19:38 +0300

    Replace hardcoded credentials with placeholders

--- Infrastructure/Data/AppDbContextFactory.cs
+++ Infrastructure/Data/AppDbContextFactory.cs
@@ -8,8 +8,7 @@ namespace Infrastructure.Data
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=jobqueuedb;Username=postgres;Password=postgres");
-
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=name_database;Username=your_username;Password=your_password_here");
             return new AppDbContext(optionsBuilder.Options);
         }
     }
